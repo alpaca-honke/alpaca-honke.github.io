860cba7cce85eeb7c73c2cb4abddd5de714e66aa
alpaca-honke <blacktea1220@hotmail.com>
2023-01-30 14:06:57 +0900

    Update ade-music to add new-release music (movie ID has not added yet)

--- ade-music/ade-music.js
+++ ade-music/ade-music.js
@@ -1,6 +1,6 @@
-const videoid = ["sm37910958","sm38620406","sm38937833","sm39126100","sm39469505","sm39759354","sm39911812","sm40238726","sm40347559","sm40515900","sm40823885","sm41190526","sm41410666"];
+const videoid = ["sm37910958","sm38620406","sm38937833","sm39126100","sm39469505","sm39759354","sm39911812","sm40238726","sm40347559","sm40515900","sm40823885","sm41190526","sm41410666","sm"];
 //下の配列は使わないけどなんとなく作ってある。
-const videotitle = ["【ネタ曲】チーズ牛丼には罪はない feat. AIきりたん / Adeliae","ディスコネクト feat. IA / Adeliae","ユモレスク feat. IA / Adeliae","ユメにサヨナラを feat. 歌愛ユキ / Adeliae","ハンパナプラス feat. IA / Adeliae","人生なんて feat. AIきりたん / Adeliae","不時着 feat. 可不 / Adeliae","シビレキラシテ (short ver.) / 可不","アンコール feat. 可不 / Adeliae","「Blink」feat. 初音ミク / Adeliae","シンフォニア feat. 初音ミク / Adeliae","スペクトラム・ケプストラム feat. 星界 / Adeliae","ハルシネイション・シザーハンズ feat. IA / Adeliae"];
+const videotitle = ["【ネタ曲】チーズ牛丼には罪はない feat. AIきりたん / Adeliae","ディスコネクト feat. IA / Adeliae","ユモレスク feat. IA / Adeliae","ユメにサヨナラを feat. 歌愛ユキ / Adeliae","ハンパナプラス feat. IA / Adeliae","人生なんて feat. AIきりたん / Adeliae","不時着 feat. 可不 / Adeliae","シビレキラシテ (short ver.) / 可不","アンコール feat. 可不 / Adeliae","「Blink」feat. 初音ミク / Adeliae","シンフォニア feat. 初音ミク / Adeliae","スペクトラム・ケプストラム feat. 星界 / Adeliae","ハルシネイション・シザーハンズ feat. IA / Adeliae","エターナルアリア feat. 初音ミク / Adeliae"];
 //<script type="application/javascript" src="https://embed.nicovideo.jp/watch/[id]/script?w=640&h=360"></script><noscript><a href="https://www.nicovideo.jp/watch/[id]">[title]</a></noscript>
 let numbers = [];
 let shuffled_numbers = [];
@@ -27,20 +27,22 @@ const shuffleArray = (array) => {
 };
 
 //ホントはform要素のonsubmitで関数呼び出したかったんですが、return falseいれてもリロードされちゃったのでまるごとjsで実装しました。このあたり今まで何度も奮闘してるなぁ
-document.getElementById("form").addEventListener("keypress", (e)=>{
-	if(e.keyCode === 13){
+document.getElementById("form").onkeydown = (e)=>{
+	if(e.key == 'Enter'){
         enter()
     };
-});
+};
     
 function enter(){
     //変数の初期化
-	numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+	numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
     ind = 0;
 	i = 0;
-	shuffled_numbers = 0;
+	shuffled_numbers = [];
 	//ランダムに10個選出
 	shuffleArray(numbers);
+		//test
+		//console.log(numbers);
 	shuffled_numbers = numbers.slice(0,10);
 
     //動画を表示
