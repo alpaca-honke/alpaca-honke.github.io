93afafc968e5452d786c42b4a71ea53395893f19
あるかっぱ/アルパカ本家 <100285837+alpaca-honke@users.noreply.github.com>
2023-01-30 19:27:30 +0900

    Add new song url:エターナルアリア(sm41720763)

--- ade-music/ade-music.js
+++ ade-music/ade-music.js
@@ -1,4 +1,4 @@
-const videoid = ["sm37910958","sm38620406","sm38937833","sm39126100","sm39469505","sm39759354","sm39911812","sm40238726","sm40347559","sm40515900","sm40823885","sm41190526","sm41410666","sm"];
+const videoid = ["sm37910958","sm38620406","sm38937833","sm39126100","sm39469505","sm39759354","sm39911812","sm40238726","sm40347559","sm40515900","sm40823885","sm41190526","sm41410666","sm41720763"];
 //下の配列は使わないけどなんとなく作ってある。
 const videotitle = ["【ネタ曲】チーズ牛丼には罪はない feat. AIきりたん / Adeliae","ディスコネクト feat. IA / Adeliae","ユモレスク feat. IA / Adeliae","ユメにサヨナラを feat. 歌愛ユキ / Adeliae","ハンパナプラス feat. IA / Adeliae","人生なんて feat. AIきりたん / Adeliae","不時着 feat. 可不 / Adeliae","シビレキラシテ (short ver.) / 可不","アンコール feat. 可不 / Adeliae","「Blink」feat. 初音ミク / Adeliae","シンフォニア feat. 初音ミク / Adeliae","スペクトラム・ケプストラム feat. 星界 / Adeliae","ハルシネイション・シザーハンズ feat. IA / Adeliae","エターナルアリア feat. 初音ミク / Adeliae"];
 //<script type="application/javascript" src="https://embed.nicovideo.jp/watch/[id]/script?w=640&h=360"></script><noscript><a href="https://www.nicovideo.jp/watch/[id]">[title]</a></noscript>
