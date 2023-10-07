c597b8118a5370c9c2e4b81379a3b63edbee4ab0
alpaca-honke <blacktea1220@hotmail.com>
2023-10-07 12:40:42 +0900

    add make-donshare-url

---
+++ make-donshare-url/make-donshare-url.js
@@ -0,0 +1,21 @@
+let text;
+let url;
+let decoded_url;
+const donshare = new URL("https://donshare.net/share.html");
+
+function done() {
+    text = document.getElementById("text").value;
+    url = document.getElementById("url").value;
+    //エンコードを解除する
+    processed_url = encodeURI(decodeURIComponent(url));
+    share_text = `${text}\n${processed_url}`;
+    misskeyshare.searchParams.set("text",share_text);
+    
+    document.getElementById("result").value = misskeyshare.href;
+}
+
+function copy() {
+    navigator.clipboard.writeText(document.getElementById("result").value).then(() => {
+        alert("コピーされました!")
+    })
+}
